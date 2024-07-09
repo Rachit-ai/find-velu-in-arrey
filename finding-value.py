1bb653454fd89787f23eb6a6c448513ff2044914
Rachit <rachit.bjp.org@gmail.com>
2024-07-09 15:28:41 +0530

    Create finding-value.py

---
+++ finding-value.py
@@ -0,0 +1,10 @@
+index = -1
+for i in range(len(arr)):
+    if arr[i] == key:
+        index = i
+        break
+
+if index != -1:
+    print(f"Positon in Array: {index +1}")
+else:
+    print("Not Found in Array 😒")
